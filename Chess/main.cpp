76f6c06c750da8fbfce3fa07cf575e04eb543150
tranhoangduong1994 <tranhoangduong1994@gmail.com>
2017-08-20 20:31:15 +0700

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Tran Hoang Duong on 8/20/17.
+//  Copyright © 2017 Tran Hoang Duong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
