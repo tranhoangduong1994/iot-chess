576ab311dbb912d2277a3eb9f3e70fa79cec6ff2
tranhoangduong <tranhoangduong1994@gmail.com>
2017-12-05 02:18:41 +0000

    small refactorings

--- Chess/GameController/Game/OfflineGame.cpp
+++ Chess/GameController/Game/OfflineGame.cpp
@@ -14,17 +14,6 @@
 
 #include "thread.h"
 
-//const std::string BOARD[] = {
-//    "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8",
-//    "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8",
-//    "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8",
-//    "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8",
-//    "E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8",
-//    "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8",
-//    "G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8",
-//    "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8",
-//};
-
 const BaseTypes::Bitboard BOARD_INIT_STATE("1111111111111111000000000000000000000000000000001111111111111111");
 
 OfflineGame::~OfflineGame() {    
@@ -45,8 +34,7 @@ void OfflineGame::start(BaseTypes::Side side, int difficulty) {
     engine->start(difficulty);
     validator->start();
     
-    gameState = GameState::INIT_STATE_VALIDATING;
-    BoardServices::getInstance()->scan();
+    BoardServices::getInstance()->resetBoard();
 }
 
 void OfflineGame::startPlayerTurn() {
@@ -104,11 +92,14 @@ void OfflineGame::onBoardStateChanged(const std::string& boardState) {
     onScanDone(boardState);
 }
 
-void OfflineGame::onBoardResetted() {
+void OfflineGame::onBoardResetted() {	
     engine->start(this->difficulty);
     validator->start();
     
     delegate->onGameStarted(GameStartedData(side));
+    
+    gameState = GameState::INIT_STATE_VALIDATING;
+    BoardServices::getInstance()->scan();
     
     if (side == BaseTypes::Side::WHITE) {
         startPlayerTurn();
